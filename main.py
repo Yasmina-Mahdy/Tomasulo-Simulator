3f6186eda86428fa300bc370d0a4bdb7fe87121f
rana5679 <124688927+rana5679@users.noreply.github.com>
2024-12-07 19:59:35 +0200

    updating main

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import ReservationStation as rs
 import ROB
 import RegFile as rf
 
-Load1 = rs.LoadRS('Load1','LOAD')
-Load2 = rs.LoadRS('Load2','LOAD')
-Store = rs.StoreRS('Store','STORE')
-BEQ = rs.BEQRS('BEQ','BEQ')
-CALLRET = rs.CallRetRS('CALLRET')
+Load1 = rs.LRS('Load1','LOAD')
+Load2 = rs.LRS('Load2','LOAD')
+Store = rs.SRS('Store','STORE')
+BEQ = rs.BRS('BEQ','BEQ')
+CALLRET = rs.CRRS('CALLRET')
 ADD1 = rs.ALRS('ADD1','ADD')
 ADD2 = rs.ALRS('ADD2','ADD')
 ADD3 = rs.ALRS('ADD3','ADD')
@@ -66,7 +66,7 @@ def instructions(inst):
         }
     return insts
 
-def parseinsts(inst,filename):
+def parseinsts(filename):
     with open(filename, 'r') as file:
         for line in file:
             inst.append(line.strip())  # .strip() removes leading/trailing whitespace
@@ -87,78 +87,100 @@ def parseinsts(inst,filename):
 
 
 def issue(pc, inst):
-    match inst.op:
+    match inst['op']:
         case 'ADD' | 'ADDI':
-            rt = inst.rt if inst.op == 'ADD' else inst.imm
+            rt = inst['rt'] if inst['op'] == 'ADD' else inst['imm']
             for r in RS:
-                if r.op == 'ADD' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rd, inst.rs, inst.rt, inst.op)
+                if r.op == 'ADD' and not r.isBusy():
+                    r.issue(inst['rd'], inst['rs'], inst['rt'], inst['op'])
                     return True
             return False
 
         case 'NAND':
             for r in RS:
-                if r.op == 'NAND' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rd, inst.rs, inst.rt, inst.op)
+                if r.op == 'NAND' and not r.isBusy():
+                    r.issue(inst['rd'], inst['rs'], inst['rt'], inst['op'])
                     return True
             return False
         
         case 'MUL':
             for r in RS:
-                if r.op == 'MUL' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rd, inst.rs, inst.rt, inst.op)
+                if r.op == 'MUL' and not r.isBusy():
+                    r.issue(inst['rd'], inst['rs'], inst['rt'], inst['op'])
                     return True
             return False
         
         case 'LOAD':
             for r in RS:
-                if r.op == 'LOAD' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rs, inst.rd, inst.imm)
+                if r.op == 'LOAD' and not r.isBusy():
+                    r.issue(inst['rs'], inst['rd'], inst['imm'])
                     return True
             return False
         
         case 'STORE':
             for r in RS:
-                if r.op == 'STORE' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rs, inst.rt, inst.imm)
+                if r.op == 'STORE' and not r.isBusy():
+                    r.issue(inst['rs'], inst['rt'], inst['imm'])
                     return True
                 
             return False
         case 'BEQ':
             for r in RS:
-                if r.op == 'BEQ' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.rs, inst.rt, pc, inst.imm)
+                if r.op == 'BEQ' and not r.isBusy():
+                    r.issue(inst['rs'], inst['rt'], pc, inst['imm'])
                     return True
             return False
         
         case 'CALL' | 'RET':
             for r in RS:
-                if r.op == 'CR' and not r.isBusy() and ROB.Reorderbuffer.isFree():
-                    r.issue(inst.op, pc, inst.imm)
+                if r.op == 'CR' and not r.isBusy():
+                    r.issue(inst['op'], pc, inst['imm'])
                     return True
             return False
 
-instList = []
-RS = []
+instList = parseinsts(filename)
+RS = [Load1, Load2, Store, BEQ, CALLRET, ADD1, ADD2, ADD3, ADD4, NAND1, NAND2, MUL]
 cycles = 0
 pc = 0 # actually modify it to be the value passed at the beginning
 offset = pc
 while(instList and not ROB.Reorderbuffer.isEmpty()):
+    can_issue = ROB.Reorderbuffer.isFree()
 
     # we have a free bus
     can_write = True
+
+    ROB.Reorderbuffer.commit()
 
     # try to advance every RS
     for r in RS:
         # if an RS is writing, it blocks all proceeding RSs
         # note that once can_write is set, we know that this rs is writing
+        prev_state = r.current_state
         can_write = not r.proceed(can_write)
+        if r.current_state == 'written' and r.current_state != prev_state:
+            written = r
         # handle writing
+    
+
+    for r in RS:
+        if r.Qj == written.Dest:
+            r.Qj = 0
+            r.Vj = ROB.Reorderbuffer.getValueself(written.Dest, written.name)
+
+        if r.Qk == written.Dest:
+            r.Qk = 0
+            r.Vk = ROB.Reorderbuffer.getValueself(written.Dest, written.name)
+
+    
+
 
     inst = instList[pc - offset] # how to deal with the offset for the list
 
+
     # trying to issue
-    if(issue(pc, inst)):
-        pc += 1
+    if can_issue:
+        if issue(pc, inst):
+            pc += 1
     
     cycles += 1
+
